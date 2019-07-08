99f97a1e0f4cc475cc7f8e56ce0547cbd17ef6b9
Gokul Reddy <gokul.reddy28@gmail.com>
2019-07-07 21:46:48 -0700

    Basic Project structure with a list and Card component

---
+++ trello-clone/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello!!!</h2>
+      <TrelloList title="Test" />
+    </div>
+  );
+}
+
+export default App;
